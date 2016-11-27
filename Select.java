d5ca4a9770c6da48c4f0d43366b84603b339e4fe
hotriluc <hotriluc97@gmail.com>
2016-11-27 20:13:41 +0200

    Update Select.java
    Class select extends JFrame

--- Select.java
+++ Select.java
@@ -9,43 +9,35 @@ import javax.swing.JLabel;
 import java.util.*;
 import javax.swing.JTextField;
 
-public class Select {
+public class Select extends JFrame {
 
 	private JFrame frmpuzzle;
 	private JTextField txtName;
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Select window = new Select();
-					window.frmpuzzle.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+	
+			
+					JFrame frame = new Select();
+					frame.setVisible(true);
+			
+	
 	}
 	public Select() {
-		initialize();
-	}
-	private void initialize() {
-		frmpuzzle = new JFrame();
-		frmpuzzle.setTitle("15_puzzle");
+	super("15_puzzle");
 		
-		frmpuzzle.setBounds(200, 200, 325, 300);
-		frmpuzzle.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmpuzzle.getContentPane().setLayout(null);
+		setBounds(200, 200, 325, 300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
 		
-		JButton btnPlay = new JButton("Play");
+		JButton btnPlay = new JButton("Select");
 		btnPlay.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				frmpuzzle.setVisible(false);
-				Puzzle_Game g = new Puzzle_Game();
-				g.setVisible(true);
+				setVisible(false);
+				MainMenu m = new MainMenu();
+				m.setVisible(true);
 			}
 		});
 		btnPlay.setBounds(10, 186, 96, 23);
-		frmpuzzle.getContentPane().add(btnPlay);
+		getContentPane().add(btnPlay);
 		
 		JButton btnExit = new JButton("Exit");
 		btnExit.addActionListener(new ActionListener() {
@@ -54,31 +46,34 @@ public class Select {
 			}
 		});
 		btnExit.setBounds(10, 220, 287, 30);
-		frmpuzzle.getContentPane().add(btnExit);
+		getContentPane().add(btnExit);
 		
 		JList prof_list = new JList();
 		prof_list.setBounds(10, 41, 287, 106);
-		frmpuzzle.getContentPane().add(prof_list);
+		getContentPane().add(prof_list);
 		
 		JLabel lblSelectProfile = new JLabel("Select profile:");
 		lblSelectProfile.setBounds(10, 16, 143, 14);
-		frmpuzzle.getContentPane().add(lblSelectProfile);
+		getContentPane().add(lblSelectProfile);
 		
 		JButton btnNew = new JButton("New");
 		btnNew.setBounds(105, 186, 103, 23);
-		frmpuzzle.getContentPane().add(btnNew);
+		getContentPane().add(btnNew);
 		
 		JButton btnDel = new JButton("Delete");
 		btnDel.setBounds(208, 186, 89, 23);
-		frmpuzzle.getContentPane().add(btnDel);
+		getContentPane().add(btnDel);
 		
 		txtName = new JTextField();
 		txtName.setBounds(52, 155, 245, 20);
-		frmpuzzle.getContentPane().add(txtName);
+		getContentPane().add(txtName);
 		txtName.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("Name:");
 		lblNewLabel.setBounds(10, 158, 46, 14);
-		frmpuzzle.getContentPane().add(lblNewLabel);
+		getContentPane().add(lblNewLabel);
 	}
+	
+	
+
 }
